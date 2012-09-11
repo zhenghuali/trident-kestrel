e322eee47e6a003645709c3bc9c8a77895de2997
Nathan Marz <nathan@twitter.com>
2012-09-11 15:59:00 -0700

    setup build

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject trident-kestrel "0.0.1-SNAPSHOT"
+  :source-path "src/clj"
+  :java-source-path "src/jvm"
+  :javac-options {:debug "true" :fork "true"}
+  :repositories {"twitter-public" "http://maven.twttr.com/"
+                 }
+
+  :dependencies [[com.twitter/finagle-kestrel "5.3.9"]
+                 ]
+
+  :dev-dependencies [[storm "0.8.1"]
+                     [org.clojure/clojure "1.4.0"]
+                     ])
+

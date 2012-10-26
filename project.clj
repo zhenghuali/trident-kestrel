4f1ce6cc547d0672c7b7bb432c4b7402ef62c847
Zhenghua Li <zhenghua@twitter.com>
2012-10-25 20:07:45 -0700

    fixed package name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trident-kestrel "0.0.3-SNAPSHOT"
+(defproject storm/trident-kestrel "0.0.3-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
